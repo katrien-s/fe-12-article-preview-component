409ee3d76b75b3b3c100c3608445e3553a401bd4
Katrien S <ontwerp@katriens.be>
2025-02-11 15:58:51 +0100

    Add in A11y

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const shareButtonElement = document.getElementById('share-btn');
 const shareSocialMediaElement = document.getElementById('share-social-media');
 
 function shareSocialMedia() {
-  shareSocialMediaElement.classList.toggle('active');
+	shareSocialMediaElement.classList.toggle('active');
+
+	const isExpanded = shareSocialMediaElement.classList.contains('active');
+	shareButtonElement.setAttribute('aria-expanded', isExpanded);
 }
 
 shareButtonElement.addEventListener('click', shareSocialMedia);
